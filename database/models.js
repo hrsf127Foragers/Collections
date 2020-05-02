ae0525e444754fbcad67f47052f854eaaf4f55fb
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-01 23:26:43 -0700

    Update models and collections to fix data type errors

--- database/models.js
+++ database/models.js
@@ -1,11 +1,12 @@
 const db = require('./connect.js');
 
 const insertRestaurant = (restaurant, callback) => {
-  let restQuery = 'INSERT INTO restaurants VALUES(0, ?, ?, ?, ?, ?, ?, NULL, ?)';
+  let restQuery = 'INSERT INTO restaurants VALUES(0, ?, ?, ?, ?, ?, NULL, ?)';
   let restArgs = [restaurant.name, restaurant.type, restaurant.rating, restaurant.num_reviews, restaurant.city, restaurant.price_range];
 
-  db.query(restQuery, restArgs, (err, results) => {
+  db.connection.query(restQuery, restArgs, (err, results) => {
     if (err) {
+      console.log('Logging insertion error from rest => ', err);
       callback(err);
     }
     callback(null, results);
@@ -14,16 +15,16 @@ const insertRestaurant = (restaurant, callback) => {
 
 // Insert a collection into the collections table
 const insertCollection = (collection, callback) => {
-  let connQuery = 'INSERT INTO collections VALUES(0, ?, ?, ?, ?, ?, ?, NULL)';
+  let connQuery = 'INSERT INTO collections (coll_name, user_creator, coll_followers, last_update, user_followers, user_ratings, user_img_url) VALUES(?, ?, ?, ?, ?, ?, NULL)';
   let connArgs = [collection.name, collection.creator, collection.coll_followers, collection.last_update, collection.user_followers, collection.user_ratings];
 
-  db.query(connQuery, connArgs, (err, results) => {
+  db.connection.query(connQuery, connArgs, (err, results) => {
     if (err) {
+      console.log('inserting collection into db error => ', err);
       callback(err);
     }
     callback(null, results);
   });
-
 };
 
 // Insert a restaurant and collection pairing into restaurants_collections table
@@ -31,7 +32,7 @@ const insertPairing = (restID, collID, callback) => {
   let joinQuery = 'INSERT INTO restaurants_collections VALUES(0, ?, ?)';
   let joinArgs = [restID, collID];
 
-  db.query(joinQuery, joinArgs, (err, results) => {
+  db.connection.query(joinQuery, joinArgs, (err, results) => {
     if (err) {
       callback(err);
     }
