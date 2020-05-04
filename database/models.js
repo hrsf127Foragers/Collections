19fb905833b14ea1827b228e54ddd9713565f1fe
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-04 10:42:51 -0700

    Refactor retrieveRestaurants and retrieveCollections to use inner joins

--- database/models.js
+++ database/models.js
@@ -42,64 +42,25 @@ const insertPairing = (restID, collID, callback) => {
 
 // Retrieve all restaurants in a given collection
 const retrieveRestaurants = (collID, callback) => {
-  // Retrieve collection id
-  let collIdQuery = `SELECT restaurant_id FROM restaurants_collections WHERE collection_id = ${collID}`;
+  let collIdQuery = `SELECT restaurants.* FROM restaurants INNER JOIN restaurants_collections WHERE restaurants_collections.collection_id = ${collID} AND restaurants_collections.restaurant_id = restaurants.id`;
 
   db.connection.query(collIdQuery, (err, results) => {
     if (err) {
       callback(err);
     }
-    // Results are an array of objects with restaurant_ids of restaurants that are part of input collection
-    const restIDs = results;
-
-    // Storage for restaurant details, will callback with this array when each restaurant has been pulled from the restaurants table
-    const restaurantDetails = [];
-    restIDs.forEach(restID => {
-      let restQuery = 'SELECT * FROM restaurants WHERE id = ?';
-      let restArgs = [restID.restaurant_id];
-      db.connection.query(restQuery, restArgs, (err, details) => {
-        if (err) {
-          console.log('Logging error from select query => ', err);
-          callback(err);
-        }
-        console.log('Logging return details from select => ', details);
-        restaurantDetails.push(details[0]);
-        // Once restaurantDetails is the same length as the return array of restaurant IDs that we were iterating through, we know we've gotten the details for every restaurant and can pass those details to our callback
-        if (restaurantDetails.length === restIDs.length) {
-          callback(null, restaurantDetails);
-        }
-      });
-    });
+    callback(null, results);
   });
 };
 
 // Retrieve all collections for a given restaurant
-// We have a restaurant ID
-// We need to make a select query on the join table for all collections linked to that restaurant ID
-// Then, pull info of all collections from the collections table
 const retrieveCollections = (restaurantID, callback) => {
-  let getCollectionIDsQuery = `SELECT collection_id FROM restaurants_collections WHERE restaurant_id = ${restaurantID}`;
+  let getCollectionIDsQuery = `SELECT collections.* FROM collections INNER JOIN restaurants_collections ON restaurants_collections.restaurant_id = ${restaurantID} AND collections.id = restaurants_collections.collection_id`;
 
   db.connection.query(getCollectionIDsQuery, (err, results) => {
     if (err) {
       callback(err);
     }
-    // Now we have all the collection IDs, iterate through them and get more details for each from collections table
-    // [{id: 1}, {id: 2}, etc.]
-    let collectionDetails = [];
-
-    results.forEach(result => {
-      let getCollectionDetailsQuery = `SELECT * FROM collections WHERE id = ${result.collection_id}`;
-      db.connection.query(getCollectionDetailsQuery, (err, results2) => {
-        if (err) {
-          callback(err);
-        }
-        collectionDetails.push(results2[0]);
-        if (collectionDetails.length === results.length) {
-          callback(null, collectionDetails);
-        }
-      });
-    });
+    callback(null, results);
   });
 };
 
