f8c650834300cf7d833f5182f7860523fc1ba40c
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-06 22:16:01 -0700

    Add 2 additional unit tests to App

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,5 +1,7 @@
 import App from '../client/src/components/App.jsx';
 import CollectionList from '../client/src/components/CollectionList.jsx';
+import Modal from '../client/src/components/Modal/Modal.jsx';
+import Title from '../client/src/components/App.jsx';
 
 import {mount, shallow} from 'enzyme';
 
@@ -12,6 +14,16 @@ describe('<App />', () => {
   it ('renders one <CollectionList /> component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(CollectionList)).toHaveLength(1);
+  });
+
+  it ('renders one <Modal /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Modal)).toHaveLength(1);
+  });
+
+  it ('sets the value of restaurantID in its initial state to the value that it\'s passed when rendered from index', () => {
+    const wrapper = shallow(<App restID={2} />);
+    expect(wrapper.state('restaurantID')).toEqual(2);
   });
 
 });
