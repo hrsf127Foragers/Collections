76f6c488236ad4b654cc7f492690b963651ee4da
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-07 11:10:59 -0700

    Add overlay to bottom right image in gallery

--- client/src/components/Modal/ModalGallery.jsx
+++ client/src/components/Modal/ModalGallery.jsx
@@ -4,6 +4,7 @@
 // Columns: 1fr 1fr 1fr 1fr
 import React from 'react';
 import styled from 'styled-components';
+import {Bookmark} from '@styled-icons/boxicons-solid/Bookmark';
 
 const ImageContainer = styled.div`
   width: 100%;
@@ -11,7 +12,6 @@ const ImageContainer = styled.div`
   top: 0;
   left: 0;
   position: absolute;
-  border: 1px solid black;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -26,10 +26,73 @@ const GalleryImage = styled.img`
   height: 100%;
 `;
 
+const OverlayContainer = styled.div`
+  position: relative;
+  grid-area: square4;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const BottomRightImage = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: blur(5px);
+`;
+
+const CornerImageOverlay = styled.p`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas:
+    "bookmark count";
+  align-items: center;
+`;
+
+const CollectionCount = styled.span`
+  font-weight: 400px;
+  grid-area: count;
+  color: white;
+  font-size: 24px;
+  justify-self: start;
+  font-family: Open Sans,Helvetica Neue,Helvetica,Arial,sans-serif;
+  margin-left: 3px;
+`;
+
+const ModalBookmarkIcon = styled(Bookmark)`
+  color: white;
+  grid-area: bookmark;
+  height: 30px;
+  width: 30px;
+  justify-self: end;
+  margin-right: 3px;
+`;
+
 const ModalGallery = (props) => (
   <ImageContainer>
     {props.images.map((image, i) => {
-      return <GalleryImage i={`square${i}`} src={image}/>
+      if (i === 4) {
+        return (
+          <OverlayContainer>
+            <BottomRightImage src={image}/>
+            <CornerImageOverlay>
+              <ModalBookmarkIcon></ModalBookmarkIcon>
+              <CollectionCount>10</CollectionCount>
+            </CornerImageOverlay>
+          </OverlayContainer>
+        );
+      } else {
+        return <GalleryImage i={`square${i}`} src={image}/>
+      }
     })}
   </ImageContainer>
 );
